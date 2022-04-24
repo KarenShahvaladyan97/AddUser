9e6a64199634c3565e3e4d8f697b0a582c465518
KarenShahvaladyan97 <93144530+KarenShahvaladyan97@users.noreply.github.com>
2022-04-24 19:32:40 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,282 @@
+let json = [{
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar5.jpg",
+            "age": 39,
+            "name": "Sykes Haley",
+            "gender": "male",
+            "email": "sykeshaley@exosis.com",
+            "phone": "+1 (912) 534-3293",
+            "address": "229 Ash Street, Waumandee, Illinois, 6624",
+            "about": "Eiusmod elit do anim ad amet nulla excepteur amet eu. Aliquip minim nulla dolore in ullamco veniam. Ullamco pariatur anim cillum adipisicing ea aute. Incididunt amet dolor ipsum in. Eu proident consectetur laboris qui Lorem officia culpa.\r\n",
+            "position": "SMM",
+            "hobby": "Watch movie"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar1.jpg",
+            "age": 31,
+            "name": "Mueller Ayala",
+            "gender": "male",
+            "email": "muellerayala@exosis.com",
+            "phone": "+1 (942) 476-3552",
+            "address": "397 Irving Street, Gulf, South Dakota, 4226",
+            "about": "Ullamco veniam reprehenderit occaecat sit velit. Occaecat nisi eiusmod exercitation minim magna sit nisi. Cillum ipsum ex ea pariatur in ex voluptate fugiat nulla Lorem esse Lorem veniam. Dolore Lorem ullamco fugiat veniam labore aliquip sit voluptate. Sit ea ea amet occaecat velit esse qui culpa esse velit laboris. Dolor nulla laboris sint aliquip est qui commodo consequat culpa do veniam non.\r\n",
+            "position": "Developer",
+            "hobby": "Play music"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar2.jpg",
+            "age": 29,
+            "name": "Amparo Perry",
+            "gender": "female",
+            "email": "amparoperry@exosis.com",
+            "phone": "+1 (802) 421-3032",
+            "address": "289 Fleet Walk, Vernon, New Jersey, 5968",
+            "about": "Aliqua ipsum cillum labore tempor consequat tempor anim reprehenderit adipisicing excepteur duis duis. Nisi nostrud aute magna ad mollit laboris duis Lorem Lorem. Mollit fugiat proident consequat consequat reprehenderit duis est consequat occaecat.\r\n",
+            "position": "HR",
+            "hobby": "Play football"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar3.jpg",
+            "age": 31,
+            "name": "Lori Guy",
+            "gender": "female",
+            "email": "loriguy@exosis.com",
+            "phone": "+1 (974) 444-3717",
+            "address": "748 Keap Street, Sims, Nebraska, 4294",
+            "about": "Enim eiusmod voluptate nisi duis culpa aliquip anim. Reprehenderit Lorem est eiusmod veniam officia magna velit deserunt dolore aliquip incididunt do. Voluptate cillum nulla cillum sit incididunt nisi esse quis proident mollit commodo. Voluptate ipsum pariatur cillum ea quis incididunt mollit eu ullamco dolore nostrud commodo.\r\n",
+            "position": "Marketing Manager",
+            "hobby": "Watch movie"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar4.jpg",
+            "age": 22,
+            "name": "Gwen Hurley",
+            "gender": "female",
+            "email": "gwenhurley@exosis.com",
+            "phone": "+1 (837) 489-2437",
+            "address": "881 Kings Place, Winchester, Arizona, 3575",
+            "about": "Eu duis excepteur nulla irure minim commodo officia ipsum. Nostrud deserunt proident anim consectetur aute culpa nostrud et in enim est dolor officia. Officia qui laborum in officia sint sunt ad ex occaecat nostrud excepteur in. Magna ut labore sint ullamco ut in velit anim sit excepteur exercitation reprehenderit consectetur.\r\n",
+            "position": "CTO",
+            "hobby": "Play football"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar5.jpg",
+            "age": 29,
+            "name": "Queen Cox",
+            "gender": "female",
+            "email": "queencox@exosis.com",
+            "phone": "+1 (811) 589-2161",
+            "address": "307 Hazel Court, Wyano, Northern Mariana Islands, 8498",
+            "about": "Qui ipsum duis sunt esse quis officia enim irure ipsum elit incididunt sit magna. Cillum sint labore voluptate cupidatat fugiat fugiat. Pariatur consequat sunt eu enim ad est eiusmod laboris ipsum occaecat ex reprehenderit exercitation magna.\r\n",
+            "position": "HR",
+            "hobby": "Play football"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar6.jpg",
+            "age": 24,
+            "name": "George Rojas",
+            "gender": "male",
+            "email": "georgerojas@exosis.com",
+            "phone": "+1 (938) 400-2654",
+            "address": "238 Dorchester Road, Martinez, Kentucky, 2446",
+            "about": "Cupidatat Lorem anim sunt mollit et nostrud consectetur culpa laborum anim elit ex ullamco sint. Occaecat aute irure nostrud nulla incididunt quis in incididunt do sit esse et. Ad voluptate in in cillum duis eu mollit aliquip ad. Nisi amet dolore deserunt magna aliqua sint dolore consequat. Laboris dolore fugiat aliquip dolor deserunt exercitation exercitation est. Nulla officia do adipisicing labore aliquip sunt occaecat ex nostrud tempor ipsum.\r\n",
+            "position": "Tech-Lead",
+            "hobby": "Watch movie"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar7.jpg",
+            "age": 29,
+            "name": "Brooks Mccullough",
+            "gender": "male",
+            "email": "brooksmccullough@exosis.com",
+            "phone": "+1 (916) 474-2599",
+            "address": "381 Roosevelt Court, Grandview, Idaho, 8144",
+            "about": "Lorem aute tempor esse proident reprehenderit irure qui voluptate eu culpa dolor ut. Et cupidatat fugiat ut voluptate ullamco laborum ad ex sunt pariatur id labore. Fugiat incididunt ipsum excepteur voluptate anim occaecat occaecat nostrud eiusmod dolor ex voluptate aliquip.\r\n",
+            "position": "HR",
+            "hobby": "Play music"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar1.jpg",
+            "age": 31,
+            "name": "Mcgowan Dodson",
+            "gender": "male",
+            "email": "mcgowandodson@exosis.com",
+            "phone": "+1 (927) 447-2412",
+            "address": "954 Holt Court, Allensworth, Wisconsin, 9633",
+            "about": "Officia eu officia ullamco ut mollit. Aute ipsum adipisicing do ullamco laboris magna tempor cupidatat. Irure eiusmod id eiusmod cillum minim incididunt est qui proident anim pariatur cillum quis. Voluptate exercitation incididunt minim laborum Lorem sint exercitation nulla sit laboris. Ex nostrud proident qui non in voluptate sunt.\r\n",
+            "position": "Marketing Manager",
+            "hobby": "Play football"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar2.jpg",
+            "age": 26,
+            "name": "Lynn Calderon",
+            "gender": "male",
+            "email": "lynncalderon@exosis.com",
+            "phone": "+1 (921) 406-3380",
+            "address": "949 Fillmore Place, Driftwood, Palau, 1831",
+            "about": "Nulla eu reprehenderit minim Lorem cupidatat est aliqua ullamco in ut irure reprehenderit cupidatat officia. Tempor est exercitation commodo laborum consectetur nostrud sint incididunt laborum esse laboris labore officia cupidatat. Ea eiusmod est laboris culpa adipisicing labore. Cupidatat ipsum velit magna do id excepteur est aliquip dolore ad velit veniam quis nisi.\r\n",
+            "position": "HR",
+            "hobby": "Watch movie"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar3.jpg",
+            "age": 40,
+            "name": "Davenport Dawson",
+            "gender": "male",
+            "email": "davenportdawson@exosis.com",
+            "phone": "+1 (950) 440-2652",
+            "address": "245 Erasmus Street, Lydia, South Carolina, 2669",
+            "about": "Ad tempor ea enim sit ad adipisicing cupidatat nulla commodo. Minim ipsum pariatur est voluptate culpa anim. Eu ullamco ipsum commodo velit occaecat minim reprehenderit commodo ea sunt velit sint voluptate. Laborum magna reprehenderit id ad voluptate in tempor minim ut. Aliqua laboris consequat velit aliquip. Pariatur incididunt dolore et velit duis. Adipisicing proident ex tempor culpa minim Lorem cillum exercitation cillum ut culpa cupidatat.\r\n",
+            "position": "Tech-Lead",
+            "hobby": "Play football"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar4.jpg",
+            "age": 27,
+            "name": "Mccoy Horn",
+            "gender": "male",
+            "email": "mccoyhorn@exosis.com",
+            "phone": "+1 (998) 421-2491",
+            "address": "367 Cox Place, Urie, Ohio, 1051",
+            "about": "Ad aute nisi dolore laboris officia dolor ut deserunt nulla magna laboris aute incididunt ullamco. Sit quis eu Lorem et cillum dolor non adipisicing cillum veniam consequat aliquip dolore. Aliquip labore ipsum reprehenderit nulla veniam. Eiusmod ut esse voluptate deserunt ad ullamco ullamco enim ea officia laborum. Aliqua nostrud consectetur dolore veniam ullamco do consequat id labore ipsum proident.\r\n",
+            "position": "Developer",
+            "hobby": "Watch movie"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar5.jpg",
+            "age": 21,
+            "name": "Leonard Franco",
+            "gender": "male",
+            "email": "leonardfranco@exosis.com",
+            "phone": "+1 (996) 514-2803",
+            "address": "323 Metropolitan Avenue, Hemlock, Alabama, 3555",
+            "about": "Anim cupidatat fugiat labore tempor exercitation sit. Commodo ut occaecat et ipsum do amet occaecat pariatur voluptate qui. Ad deserunt dolor veniam elit pariatur aliqua nulla sit fugiat.\r\n",
+            "position": "Developer",
+            "hobby": "Watch movie"
+        },
+        {
+            "picture": "https://ericssonr.nsdbytes.com/assets/images/sm/avatar6.jpg",
+            "age": 32,
+            "name": "Simpson Shannon",
+            "gender": "male",
+            "email": "simpsonshannon@exosis.com",
+            "phone": "+1 (982) 419-2393",
+            "address": "967 Estate Road, Jardine, New York, 1000",
+            "about": "Et deserunt dolore deserunt eiusmod eiusmod et ad nisi cupidatat tempor anim. Consectetur nisi velit pariatur elit pariatur nisi proident enim sunt occaecat. Et ut anim veniam aliquip. Nulla eiusmod cupidatat consequat enim cupidatat ipsum ea sit qui eu dolore qui voluptate sint.\r\n",
+            "position": "CTO",
+            "hobby": "Watch movie"
+        }
+    ]
+    //create users
+function createUsers(obj) {
+    let person = ''
+    for (let i = 0; i < obj.length; i++) {
+        person += `<div class="person id='${i}'">
+        <div class="imgEditEye">
+            <div class="img" style="background-image: url(${obj[i].picture});"></div>
+            <span class="edit" id='edit${i}' data-toggle="modal" data-target="#exampleModal"></span>
+            <span class="eye" id='eye${i}'data-toggle="modal" data-target="#exampleModal" title="View"></span>
+        </div>
+        <div class="description">
+            <p class="name">${obj[i].name}</p>
+            <p class="ceo">${obj[i].position}</p>
+            <p class="tel">${obj[i].phone}</p>
+            <p class="addres">${obj[i].address}</p>
+        </div>
+    </div>`
+    }
+    document.querySelector('.users').innerHTML = person
+}
+createUsers(json)
+
+//create view modal or edite
+function modal() {
+    for (let i = 0; i < document.querySelector('.users').children.length; i++) {
+        document.querySelector('.users').children[i].addEventListener('click', (el) => {
+
+            if (el.target.id == `edit${i}`) {
+                let chanegUser = ` <form id='changeValue'>
+                <p>Change User</p>
+                <input type="text" value="${json[i].name}">
+                <input type="text" value="${json[i].position}">
+                <input type="tel" value="${json[i].phone}">
+                <input type="text" value="${json[i].address}" maxlength="50">
+                                    </form>`
+                let save = `<button type="button" onclick = 'my(${i})'data-dismiss="modal" class="btn btn-primary" id='save'>Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>`
+                document.querySelector('.modal-footer').innerHTML = save
+                document.querySelector('.modal-user').innerHTML = chanegUser
+            }
+
+            if (el.target.id == `eye${i}`) {
+                let userData = `<div class="userData">
+                        <p>User Description</p>
+                        <p class='des'>Name : ${json[i].name}</p>
+                        <p class='des'>Gender : ${json[i].gender}</p>
+                        <p class='des'>Email : ${json[i].email}</p>
+                        <p class='des'>Phone : ${json[i].phone}</p>
+                        <p class='des'>Addres : ${json[i].address}</p>
+                        <p class='des'>About : ${json[i].about}</p>
+                        <p class='des'>Position : ${json[i].position}</p>
+                        <p class='des'>Hobby : ${json[i].hobby}</p>
+                        </div>`
+                document.querySelector('.modal-user').innerHTML = userData
+                document.getElementById("save").style.display = 'none'
+
+            }
+        })
+    }
+}
+modal()
+
+//modal function
+
+
+function addUser() {
+    let newUser = `<form id='newUser'>
+            <p>Create New User</p>
+            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Age">
+            <input type="tel" placeholder="Gender">
+            <input type="text" placeholder="E-mail">
+            <input type="text" placeholder="Phone">
+            <input type="text" placeholder="Address">
+            <input type="text" placeholder="About">
+            <input type="text" placeholder="Position">
+            <input type="text" placeholder="Hobby">
+            <input type="file" placeholder="Hobby">
+                       </form>`
+    let create = `<button type="button" onclick = 'createNewUser()'data-dismiss="modal" class="btn btn-primary">Create</button>
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>`
+    document.querySelector('.modal-footer').innerHTML = create
+    document.querySelector('.modal-user').innerHTML = newUser
+}
+
+function createNewUser() {
+    json.push({
+        picture: document.getElementById('newUser').children[10].value,
+        age: document.getElementById('newUser').children[2].value,
+        name: document.getElementById('newUser').children[1].value,
+        gender: document.getElementById('newUser').children[3].value,
+        email: document.getElementById('newUser').children[4].value,
+        phone: document.getElementById('newUser').children[5].value,
+        address: document.getElementById('newUser')[6].value,
+        about: document.getElementById('newUser').children[7].value,
+        position: document.getElementById('newUser').children[8].value,
+        hobby: document.getElementById('newUser').children[9].value
+    })
+    createUsers(json)
+    modal()
+}
+
+function my(user) {
+    let changeValue = []
+    for (let i = 0; i < document.querySelector('#changeValue').children.length; i++) {
+        changeValue.push(document.querySelector('#changeValue').children[i].value)
+    }
+    json[user].name = changeValue[1]
+    json[user].position = changeValue[2]
+    json[user].phone = changeValue[3]
+    json[user].address = changeValue[4]
+    createUsers(json)
+}
